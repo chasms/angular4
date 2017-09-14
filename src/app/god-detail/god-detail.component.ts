d83249ab91383438256244555cc6fe5b69de7296
Charles Samuels <chasms@chas.ms>
2017-09-14 15:45:35 -0400

    got detail view working with app component passing down selectedGod

--- src/app/god-detail/god-detail.component.ts
+++ src/app/god-detail/god-detail.component.ts
@@ -1,17 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { God } from '../god';
 
 @Component({
-  selector: 'app-god-detail',
+  selector: 'god-detail',
   templateUrl: './god-detail.component.html',
   styleUrls: ['./god-detail.component.css']
 })
-export class GodDetailComponent implements OnInit {
-  god: God;
-
-  constructor() {}
-
-  ngOnInit() {}
-
+export class GodDetailComponent {
+  @Input() god: God;
 }
