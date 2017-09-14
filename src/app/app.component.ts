d9e45eab8d504c805908570d8d6fb265cf1bd739
Charles Samuels <chasms@chas.ms>
2017-09-14 10:43:22 -0400

    added description

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 export class God {
   id: number;
   name: string;
+  description: string;
 }
 @Component({
   selector: 'app-root',
@@ -13,6 +14,7 @@ export class AppComponent {
   title = 'The Pantheon';
   god: God = {
     id: 1,
-    name: "Xáos"
+    name: "Xáos",
+    description: `In Greek mythology, Chaos (Greek: Χάος), according to Hesiod,Chaos ("Chasm") was the first thing to exist: "at first Chaos came to be" (or was) "but next" (possibly out of Chaos) came Gaia, Tartarus, and Eros (elsewhere the son of Aphrodite). Unambiguously born "from Chaos" were Erebus (Darkness) and Nyx (Night).`,
   };
 }
