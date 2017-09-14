17490ffba7db83470d75da79a10f42c996b0daa4
Charles Samuels <chasms@chas.ms>
2017-09-14 10:22:48 -0400

    added app details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'The Pantheon';
+  hero = "Xa'0s";
 }
+
+
